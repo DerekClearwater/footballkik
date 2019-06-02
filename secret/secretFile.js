2b7562e2a5599d9870c7234dd3f91e45c834461d
Derek Clearwater <derek.clearwater@gmail.com>
2019-06-02 09:14:07 +0100

    Completed the application

---
+++ secret/secretFile.js
@@ -0,0 +1,14 @@
+module.exports = {
+    facebook: {
+        clientID: '556799398058499',
+        clientSecret: '5165641ef01ce056428eb204e1827863',
+       // profileFields: ['email', 'displayName', 'photos'],
+       // callbackURL: 'http://localhost:3000/auth/facebook/callback',
+       // passReqToCallback: true
+    },
+    
+    google: {
+        clientID: '269731627004-utp446bhl2vi0pe2ji1b4lfnaa91ajia.apps.googleusercontent.com',
+        clientSecret: 'm7NKdgeoFTxKk90i6uA3-2qS'
+    }
+}
